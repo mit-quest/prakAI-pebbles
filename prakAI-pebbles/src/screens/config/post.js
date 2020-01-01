5e7cc486ae129096933eb8e49145075353b44a01
KyleKeane <KyleKeane@users.noreply.github.com>
2019-12-31 21:29:40 -0500

    reset settings from cache

--- prakAI-pebbles/src/screens/config/post.js
+++ prakAI-pebbles/src/screens/config/post.js
@@ -3,44 +3,64 @@ console.log("begin config/post.js");
 resetPreview();
 
 backgroundColorPicker = document.getElementById('backgroundColorPicker');
-backgroundColor = backgroundColorPicker.value;
-sessionStorage.setItem("backgroundColor", backgroundColor);
-refreshStyling();
-
 backgroundColorPicker.onchange = function(ev) {
 	sessionStorage.setItem("backgroundColor", ev.target.value);
 	refreshStyling();
 };
 
 fontColorPicker = document.getElementById('fontColorPicker');
-fontColor = fontColorPicker.value;
-sessionStorage.setItem("fontColor", fontColor);
-refreshStyling();
-
 fontColorPicker.onchange = function(ev) {
 	sessionStorage.setItem("fontColor", ev.target.value);
 	refreshStyling();
 };
 
 sizePicker = document.getElementById('sizePicker');
-size = sizePicker.value;
-sessionStorage.setItem("size", size);
-refreshStyling();
-
 sizePicker.oninput = function(ev) {
 	sessionStorage.setItem("size", ev.target.value);
 	refreshStyling();
 } 
 
 spacingPicker = document.getElementById('spacingPicker');
-spacing = spacingPicker.value;
-sessionStorage.setItem("spacing", spacing);
-refreshStyling();
-
 spacingPicker.oninput = function(ev) {
   sessionStorage.setItem("spacing", ev.target.value);
   refreshStyling();
 } 
+
+backgroundColor = sessionStorage.getItem("backgroundColor");
+
+if (backgroundColor == null) {
+	backgroundColor = backgroundColorPicker.value;
+	sessionStorage.setItem("backgroundColor", backgroundColor);
+} else {
+	backgroundColorPicker.value = backgroundColor;
+}
+
+fontColor = sessionStorage.getItem("fontColor");
+
+if (fontColor == null) {
+	fontColor = fontColorPicker.value;
+	sessionStorage.setItem("fontColor", fontColor);
+} else {
+	fontColorPicker.value = fontColor;
+}
+
+size = sessionStorage.getItem("size");
+
+if (size == null) {
+	size = sizePicker.value;
+	sessionStorage.setItem("size", size);
+} else {
+	sizePicker.value = size;
+}
+
+spacing = sessionStorage.getItem("spacing");
+
+if (spacing == null) {
+	spacing = spacingPicker.value;
+	sessionStorage.setItem("spacing", spacing);
+} else {
+	spacingPicker.value = spacing;
+}
 
 displayCachedExperiment();
 
